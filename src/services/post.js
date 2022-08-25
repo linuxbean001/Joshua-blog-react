93da17aeab771c4142e1e7d48f68e463b205e3a0
linuxbean001 <33897512+linuxbean001@users.noreply.github.com>
2022-08-25 14:22:16 +0530

    Update post.js

--- src/services/post.js
+++ src/services/post.js
@@ -3,13 +3,13 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const postApi = createApi({
     reducerPath:'postApi',
     baseQuery: fetchBaseQuery({
-        baseUrl:'https://jsonplaceholder.typicode.com/',
+        baseUrl:'http://127.0.0.1:8000/api/',
     }),
 
     endpoints: (builder) =>({
         getAllPost: builder.query({
           query: () => ({
-            url: 'posts',
+            url: 'data',
             method: 'GET'  
           })  
         })
